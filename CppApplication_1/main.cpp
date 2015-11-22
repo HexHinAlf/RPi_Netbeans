d7e890a40ccef35b0fb2d42e4ed5300442a998c0
Felix <fmm.hahn@gmail.com>
2015-11-22 23:28:55 +0100

    bessere Erkennung (LPD)

--- CppApplication_1/main.cpp
+++ CppApplication_1/main.cpp
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 
+#include "main.hpp"
+
+
 /* 
  * File:   main.cpp
  * Author: Felix
@@ -13,14 +16,6 @@
 
  /**
 */
-#include <ctime>
-#include <iostream>
-#include <raspicam/raspicam_cv.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <tesseract/baseapi.h>
-#include <leptonica/allheaders.h>
 
 //using namespace cv;
 //using namespace std;
@@ -43,10 +38,10 @@ int main(int argc, char **argv) {
     std::string cameraWindowName = "Camera";
     std::string editorWindowName = "Corrected Image";
     std::string resultWindowName = "Tesseract Output";
-
-    //cv::namedWindow(cameraWindowName, cv::WINDOW_NORMAL); // Create a window for display.
-    cv::namedWindow(editorWindowName, cv::WINDOW_NORMAL);
-    cv::namedWindow(resultWindowName, cv::WINDOW_NORMAL);
+    // Create a window for display.
+    cv::namedWindow(cameraWindowName, cv::WINDOW_NORMAL);
+    cv::namedWindow(editorWindowName, cv::WINDOW_AUTOSIZE);
+    //cv::namedWindow(resultWindowName, cv::WINDOW_NORMAL);
 
     //set camera parameter
     //Camera.set(CV_CAP_PROP_EXPOSURE, 1);
@@ -68,7 +63,7 @@ int main(int argc, char **argv) {
     Camera.retrieve(cameraImage);
     
     // create a simple window to display the video
-    //cv::imshow(cameraWindowName, cameraImage);
+    cv::imshow(cameraWindowName, cameraImage);
     
     std::cout << "Stop camera..." << std::endl;
     Camera.release();
@@ -76,32 +71,47 @@ int main(int argc, char **argv) {
     
     //edit image to greyscale
     cv::cvtColor(cameraImage,editorImage,CV_RGB2GRAY);
-    cv::imshow(editorWindowName, editorImage);
+    //cv::imshow(editorWindowName, editorImage);
     
     
     
     //convert to text
-    tess->SetImage((uchar*)editorImage.data, editorImage.size().width, editorImage.size().height, editorImage.channels(), editorImage.step1());
-    tess->Recognize(0);
-    outText = tess->GetUTF8Text();
+//    tess->SetImage((uchar*)editorImage.data, editorImage.size().width, editorImage.size().height, editorImage.channels(), editorImage.step1());
+//    tess->Recognize(0);
+//    outText = tess->GetUTF8Text();
     
     //display text
-    resultImage = cv::Mat::zeros(250,250,CV_8U);
-    cv::putText(resultImage, outText, cv::Point(0,50), CV_FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(255),1,8,false);
-    cv::imshow(resultWindowName, resultImage);
+//    resultImage = cv::Mat::zeros(250,250,CV_8U);
+//    cv::putText(resultImage, outText, cv::Point(0,50), CV_FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(255),1,8,false);
+//    cv::imshow(resultWindowName, resultImage);
     
     
-
-    //show time statistics
-//    time(&timer_end); /* get current time; same as: timer = time(NULL)  */
-//
-//    double secondsElapsed = difftime(timer_end, timer_begin);
-//    cout << secondsElapsed << " seconds for " << nCount << "  frames : FPS = " << (float) ((float) (nCount) / secondsElapsed) << endl;
-
     // save image 
     //cv::imwrite("raspicam_cv_image.jpg", image);
-    std::cout << "Image saved at raspicam_cv_image.jpg" << std::endl;
+    //std::cout << "Image saved at raspicam_cv_image.jpg" << std::endl;
     std::cout << outText << std::endl;
+    
+    
+    
+    
+
+    
+
+    /// Create a Trackbar for user to enter threshold
+    //cv::createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
+
+    /// Show the image
+    //getTextblocks(cameraImage, NULL);
+    
+    cv::Mat textMask = cv::Mat::zeros( cameraImage.size(), editorImage.type());
+    cv::Point correctionPoint = cv::Point(20,20);
+    for (auto box : detectLetters(cameraImage))
+    {
+        cv::rectangle(textMask, box.tl()-correctionPoint, box.br()+correctionPoint, cv::Scalar(255,255,255), CV_FILLED, 8, 0 );      
+    }
+    
+    editorImage.copyTo( resultImage, textMask);
+    cv::imshow(editorWindowName, resultImage);
 
     cv::waitKey(0);
     
@@ -109,4 +119,94 @@ int main(int argc, char **argv) {
     tess->End();
     delete [] outText;
     
+}
+
+
+//find blocks of text in an image using different filter techniques
+void getTextblocks(cv::Mat src, std::vector<cv::Mat> *textblocks)
+{
+    //step one: canny edge filter
+    cv::Mat dst, dst2, src_gray, detected_edges, linek;
+    
+    int lowThreshold = 60;
+    int ratio = 3;
+    int kernel_size = 3;
+    char* window_name = "Edge Map";
+    
+    /// Create a matrix of the same type and size as src (for dst)
+    dst.create( src.size(), src.type() );
+    dst2.create( src.size(), src.type() );
+    /// Convert the image to grayscale
+    cv::cvtColor( src, src_gray, CV_BGR2GRAY );
+    /// Reduce noise with a kernel 3x3
+    cv::blur( src_gray, detected_edges, cv::Size(3,3) );
+    /// Canny detector
+    cv::Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+    
+    /// Using Canny's output as a mask, we display our result
+    //dst = cv::Scalar::all(0);
+
+    //src.copyTo( dst, detected_edges);
+    
+    //Rahmen entfernen
+
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i> hierarchy;
+    
+    cv::findContours(detected_edges, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+     /// Approximate contours to polygons + get bounding rects and circles
+     std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+     std::vector<cv::Rect> boundRect( contours.size() );
+     std::vector<cv::Point2f> center( contours.size() );
+     std::vector<float> radius( contours.size() );
+
+     for( int i = 0; i < contours.size(); i++ )
+     { 
+         cv::approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true );
+         boundRect[i] = cv::boundingRect( cv::Mat(contours_poly[i]) );
+     }
+
+     /// Draw polygonal contour + bonding rects
+     cv::Mat textMask = cv::Mat::zeros( detected_edges.size(), detected_edges.type());
+     cv::Point correctionPoint = cv::Point(20,20);
+     for( int i = 0; i< contours.size(); i++ )
+     {
+         cv::Scalar color = cv::Scalar(255,0,255);
+         //cv::drawContours( drawing, contours_poly, i, color, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point() );
+         if(boundRect[i].width > 300 && boundRect[i].height > 150)
+            cv::rectangle( textMask, boundRect[i].tl()+correctionPoint, boundRect[i].br()-correctionPoint, color, CV_FILLED, 8, 0 );          
+     }
+     
+     src_gray.copyTo( dst, textMask);
+     
+    /// Create a window
+    cv::namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+    cv::imshow( window_name, dst );
+    
+
+}
+
+
+//code from stackoverflow 23506105, William
+std::vector<cv::Rect> detectLetters(cv::Mat img)
+{
+    std::vector<cv::Rect> boundRect;
+    cv::Mat img_gray, img_sobel, img_threshold, element;
+    cv::cvtColor(img, img_gray, CV_BGR2GRAY);
+    cv::Sobel(img_gray, img_sobel, CV_8U, 1, 0, 3, 1, 0, cv::BORDER_DEFAULT);
+    cv::threshold(img_sobel, img_threshold, 0, 255, CV_THRESH_OTSU+CV_THRESH_BINARY);
+    element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(30, 30) );
+    cv::morphologyEx(img_threshold, img_threshold, CV_MOP_CLOSE, element); //Does the trick
+    std::vector< std::vector< cv::Point> > contours;
+    cv::findContours(img_threshold, contours, 0, 1); 
+    std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+    for( int i = 0; i < contours.size(); i++ )
+        if (contours[i].size()>100)
+        { 
+            cv::approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true );
+            cv::Rect appRect( cv::boundingRect( cv::Mat(contours_poly[i]) ));
+            if (appRect.width>appRect.height) 
+                boundRect.push_back(appRect);
+        }
+    return boundRect;
 }
